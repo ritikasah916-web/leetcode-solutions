066c24716218389a6de2b81e755a346f5e8583a2
Ritika sah <ritikasah916@gmail.com>
2026-09-20 20:09:50 +0530

    Add Reverse Degree of a String solution

---
+++ 3498_reverse_degree_of_a_string.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseDegree(self, s: str) -> int:
+        res = 0
+        for i,ch in enumerate(s):
+            alpha_index = 26-(ord(ch)-ord('a'))
+            res+=alpha_index*(i+1)
+        return res    
+        
